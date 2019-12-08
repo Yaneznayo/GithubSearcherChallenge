64e8ef5e6a745446b5d8bdba7dfed8dfc6220880
Yaneznayo <Christopher.pung@mobileappscompany.us>
2019-12-07 19:12:10 -0500

    Initial Commit

---
+++ GitUserSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUserSearchApp
+//
+//  Created by Christopher Pung on 12/7/19.
+//  Copyright © 2019 Mobile Apps Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
