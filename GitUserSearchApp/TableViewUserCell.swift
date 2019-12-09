d80909292d7cb4cde26ed28ee52f436f869c6188
Yaneznayo <Christopher.pung@mobileappscompany.us>
2019-12-09 09:37:17 -0500

    Initial Commit

---
+++ GitUserSearchApp/TableViewUserCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewUserCell.swift
+//  GitUserSearchApp
+//
+//  Created by Christopher Pung on 12/7/19.
+//  Copyright © 2019 Mobile Apps Company. All rights reserved.
+//
+
+import UIKit
+
+class TableViewUserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
